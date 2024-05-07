dfa358c08bd322966c497bda133579b345a13cd2
rvn02 <rouven.meyer02@gmail.com>
2024-05-07 17:53:44 +0200

    commissioning of the DMD and the ESP

--- src/main.cpp
+++ src/main.cpp
@@ -1,16 +1,65 @@
+/*
+Pin connections
+DMD | ESP
+LAT - 22
+A   - 19
+B   - 23
+OE  - 16
+R   - 13
+CLK - 14
+*/
+
 #include <Arduino.h>
+#include <PxMatrix.h>
 
-#define LED_BUILTIN 12
+#define P_LAT 22
+#define P_A 19
+#define P_B 23
+#define P_OE 16
+hw_timer_t * timer = NULL;
+portMUX_TYPE timerMux = portMUX_INITIALIZER_UNLOCKED;
 
-void setup() {
-  // put your setup code here, to run once:
-  pinMode(LED_BUILTIN, OUTPUT);
+// Pins for LED MATRIX
+
+PxMATRIX display(32,16,P_LAT, P_OE,P_A,P_B);
+//PxMATRIX display(64,32,P_LAT, P_OE,P_A,P_B,P_C,P_D);
+//PxMATRIX display(64,64,P_LAT, P_OE,P_A,P_B,P_C,P_D,P_E);
+
+
+void IRAM_ATTR display_updater(){
+  // Increment the counter and set the time of ISR
+  portENTER_CRITICAL_ISR(&timerMux);
+  //isplay.display(70);
+  display.displayTestPattern(70);
+  portEXIT_CRITICAL_ISR(&timerMux);
 }
 
+
+uint16_t myCYAN = display.color565(255, 255, 255);
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(9600);
+  display.begin(4);
+  // display.setScanPattern(ZAGGIZ);
+  // display.setMuxPattern(BINARY);
+  display.flushDisplay();
+  display.setTextColor(myCYAN);
+  display.setCursor(0,0);
+  display.print("Pixel");
+  Serial.println("hello");
+
+  timer = timerBegin(0, 80, true);
+  timerAttachInterrupt(timer, &display_updater, true);
+  timerAlarmWrite(timer, 4000, true);
+  timerAlarmEnable(timer);
+
+
+  delay(1000);
+}
+
+
 void loop() {
-  // put your main code here, to run repeatedly:
-  digitalWrite(LED_BUILTIN, LOW);
-  delay(1000);
-  digitalWrite(LED_BUILTIN, HIGH);
-  delay(1000);
+
+ delay(100);
+
 }
